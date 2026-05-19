59835535e46a258a35e6a5c9bd22f873c69fd4da
Belladone-Bzz <belladone.gaming@gmail.com>
2026-05-19 19:37:27 +0200

    feat[maze_gen/maze.py]: add Cell nested class, generation and repr methods to Maze class

--- maze_gen/maze.py
+++ maze_gen/maze.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, model_validator
 from typing import Annotated
+from enum import IntEnum
 
 
 MazeDimension = Annotated[int, Field(ge=2)]
@@ -10,7 +11,23 @@ CellCoordinates = Annotated[
     Field(min_length=2, max_length=2)]
 
 
+class Directions(IntEnum):
+    """Class Directions.
+    Enum for west(0), south(1), east(2), north(3).
+    """
+    WEST = 0
+    SOUTH = 1
+    EAST = 2
+    NORTH = 3
+
+
 class Maze:
+    """Class Maze.
+    Attributes: width, height, entry, exit, perfect, seed,
+    central_icon, config, cells.
+    Methods: generation(), repr().
+    Nested_class: Config, Cell.
+    """
     def __init__(
             self, width: int, height: int,
             entry: tuple[int, int], exit: tuple[int, int],
@@ -23,8 +40,13 @@ class Maze:
             PERFECT=perfect,
             SEED=seed,
             CENTRAL_ICON=central_icon)
+        self.cells: list[list[Maze.Cell]] = []
 
     class Config(BaseModel):
+        """Class Config
+        Attributes: WIDTH, HEIGHT, ENTRY, EXIT, CENTRAL_ICON, PERFECT, SEED.
+        Methods: validate_config(), str().
+        """
         WIDTH: MazeDimension
         HEIGHT: MazeDimension
         ENTRY: CellCoordinates
@@ -36,6 +58,7 @@ class Maze:
 
         @model_validator(mode='after')
         def validate_config(self) -> "Maze.Config":
+            """Model validator for maze's configuration."""
             error_message: str = ""
             if self.CENTRAL_ICON is True and (
                     self.WIDTH < 7 or self.HEIGHT < 7):
@@ -55,6 +78,9 @@ class Maze:
             return self
 
         def __str__(self) -> str:
+            """Method to display configuration of the maze (width, height,
+            entry, exit, icon toggle, perfect toggle and seed).
+            """
             return (
                 f"WIDTH: {self.WIDTH}, HEIGHT: {self.HEIGHT}\n"
                 f"ENTRY: {self.ENTRY}, EXIT: {self.EXIT}\n"
@@ -62,17 +88,62 @@ class Maze:
                 f"PERFECT TOGGLE: {self.PERFECT}\nSEED: {self.SEED}")
 
     class Cell:
-        pass
+        """Class Cell
+        Atributes: coordinates: bool, walls: list[bool], entry: bool,
+        exit: bool, pattern: bool.
+        """
+        def __init__(self, coordinates: CellCoordinates):
+            self.coordinates: CellCoordinates = coordinates
+            self.walls: list[bool] = [False, False, False, False]
+            self.entry = False
+            self.exit = False
+            self.pattern = False
+
+    def generation(self) -> None:
+        """Method to generate all the cells in the maze grid in a
+        list[list[Maze.Cell]] Only the outer walls are set to True.
+        Entry and Exit cells are memorized.
+         """
+        for x in range(self.config.WIDTH):
+            self.cells.append([])
+            for y in range(self.config.HEIGHT):
+                self.cells[x].append(Maze.Cell((x, y)))
+        for cell in self.cells[0]:
+            cell.walls[Directions.WEST] = True
+        for cell in self.cells[-1]:
+            cell.walls[Directions.EAST] = True
+        for x in range(self.config.WIDTH):
+            self.cells[x][0].walls[Directions.NORTH] = True
+            self.cells[x][-1].walls[Directions.SOUTH] = True
+        self.cells[self.config.ENTRY[0]][self.config.ENTRY[1]].entry = True
+        self.cells[self.config.EXIT[0]][self.config.EXIT[1]].exit = True
+
+    def __repr__(self) -> str:
+        """Method to display debug mode of the maze walls."""
+        maze: str = "+---" * self.config.WIDTH + "+\n"
+        for y in range(self.config.HEIGHT):
+            maze += "|" + "".join(
+                "   |" if cell.walls[Directions.EAST] is True else "    "
+                for cell in (self.cells[x][y]
+                             for x in range(self.config.WIDTH))) + "\n"
+            maze += "+" + "".join(
+                "---+" if cell.walls[Directions.SOUTH] is True else "   +"
+                for cell in (self.cells[x][y]
+                             for x in range(self.config.WIDTH))) + "\n"
+        return maze
 
 
 if __name__ == "__main__":
-    config = Maze(
-        width=2,
-        height=2,
+    """Entry point of the program"""
+    maze = Maze(
+        width=3,
+        height=3,
         entry=(0, 0),
-        exit=(1, 1),
+        exit=(2, 2),
         perfect=True,
         seed=123456,
         central_icon=False
     )
-    print(config.config)
+    print(maze.config)
+    maze.generation()
+    print(maze)
