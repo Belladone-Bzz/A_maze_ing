588faf195a8da83d3ed67a34991b89bbdfd24105
jolyne-mangeot <jolyne.mangeot@gmail.com>
2026-05-30 17:54:24 +0200

    add write_out_maze function to a_maze_ing.py file to wriite out statistics about the maze in a given file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -79,8 +79,22 @@ def instantiate_maze(
         return message
 
 
-def write_out_maze(maze: Maze, output_file: str) -> str:
-    pass
+def write_out_maze(maze: Maze, config: dict[str, str]) -> str:
+    maze_str: str = ""
+    for y in range(maze.config.HEIGHT):
+        for x in range(maze.config.WIDTH):
+            maze_str += hex(int("".join([str(
+                int(wall)) for wall in maze.cells[x][y].walls]), 2))[2].upper()
+        maze_str += "\n"
+    maze_str += f"\n{config["entry"]}\n{config["exit"]}\n"
+    try:
+        with open(config["output_file"], 'w') as file:
+            print(maze_str, end="", file=file)
+    except FileNotFoundError:
+        return f"- Output file {config["output_file"]} not found"
+    except PermissionError:
+        return f"- Output file {config["output_file"]} could not be accessed"
+    return ""
 
 
 def main() -> int:
@@ -159,7 +173,7 @@ def main() -> int:
                     maze = new_maze
                     break
             elif menu_output == "save_maze":
-                menu_output = write_out_maze(maze, config["output_file"])
+                menu_output = write_out_maze(maze, config)
                 if menu_output != "":
                     menu_module("maze_error", menu_output)
     return 0
